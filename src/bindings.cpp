6a90984090a98e00cca348e03677f25eb687b98d
Isaac Hier <ihier@uber.com>
2017-11-08 13:59:56 -0500

    Implement span bindings

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,9 +1,81 @@
+#include <iomanip>
+#include <sstream>
 #include <pybind11/pybind11.h>
 #include <opentracing/span.h>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(python_example, m) {
+namespace {
+
+template <typename Clock>
+typename Clock::time_point timestampFromPyFloat(py::object timestamp)
+{
+    try {
+        const auto timestampDouble = py::cast<double>(timestamp);
+        const std::chrono::duration<double> timeSinceEpoch(timestampDouble);
+        return typename Clock::time_point() +
+               std::chrono::duration_cast<std::chrono::seconds>(
+                   timeSinceEpoch);
+    } catch (...) {
+        return Clock::now();
+    }
+}
+
+std::string systemTimestamp(const opentracing::SystemClock::time_point& time)
+{
+    std::ostringstream oss;
+    const auto cTime = opentracing::SystemClock::to_time_t(time);
+    std::tm tm;
+    oss << std::put_time(gmtime_r(&cTime, &tm), "%c %Z");
+    return oss.str();
+}
+
+}  // anonymous namespace
+
+PYBIND11_MODULE(opentracing_cpp, m) {
+    using namespace opentracing;
+    py::class_<SpanContext>(m, "SpanContext")
+        .def("baggage", [](const SpanContext& sc) {
+            std::unordered_map<std::string, std::string> baggage;
+            sc.ForeachBaggageItem([&baggage](
+                const std::string& key, const std::string& value) {
+                baggage[key] = value;
+                return true;
+            });
+            return baggage;
+        });
+
+    py::class_<Span>(m, "Span")
+        .def("context", &Span::context)
+        // TODO: .def("tracer", &Span::tracer)
+        .def("set_operation_name", &Span::SetOperationName)
+        .def("finish", [](Span& span, py::object finishTime) {
+            if (finishTime.is_none()) {
+                span.Finish();
+            }
+            else {
+                FinishSpanOptions options;
+                options.finish_steady_timestamp =
+                    timestampFromPyFloat<SteadyClock>(
+                        finishTime);
+                span.FinishWithOptions(options);
+            }
+        }, py::arg("finish_time") = py::none())
+        .def("set_tag", &Span::SetTag)
+        .def("log_kv", [](Span& span,
+                          py::dict keyValues,
+                          py::object timestamp) {
+            for (auto&& pair : keyValues) {
+                span.Log({{ py::cast<std::string>(pair.first),
+                            Value(py::cast<std::string>(pair.second)) }});
+            }
+            const auto logTime =
+                timestampFromPyFloat<SystemClock>(timestamp);
+            span.Log({{ "timestamp", systemTimestamp(logTime) }});
+        }, py::arg(), py::arg("timestamp") = py::none())
+        .def("set_baggage_item", &Span::SetBaggageItem)
+        .def("get_baggage_item", &Span::BaggageItem);
+
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
 #else
